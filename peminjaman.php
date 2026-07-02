21804281f98baa2f8d3114062faad35cc64f2314
Sakin Khair <khairsakin@gmail.com>
2026-07-02 13:57:25 +0700

    Add files via upload

---
+++ peminjaman.php
@@ -0,0 +1,56 @@
+<?php
+$daftarBarang = [];  
+$idBerikutnya = 1;    
+
+
+while (true) {
+    echo "\n=== SISTEM PEMINJAMAN BARANG UPATIK ===\n";
+    echo "1. Tambah  2. Lihat  3. Hapus  4. Keluar\n";
+    $pilih = readline("Pilih menu: ");
+
+
+    if ($pilih == "1") {
+        $nama = readline("Nama Barang   : ");
+        $peminjam = readline("Nama Peminjam : ");
+
+
+        $barangBaru = [
+            "id"       => $idBerikutnya,
+            "nama"     => $nama,
+            "peminjam" => $peminjam,
+            "status"   => "Dipinjam"
+        ];
+        $daftarBarang[] = $barangBaru;  
+        $idBerikutnya++;
+        echo "> Barang berhasil ditambahkan!\n";
+
+
+    } elseif ($pilih == "2") {
+        if (count($daftarBarang) == 0) {
+            echo "Belum ada barang dipinjam.\n";
+        } else {
+            $no = 1;
+            foreach ($daftarBarang as $b) {
+                echo $no . ". [ID: " . $b["id"] . "] " . $b["nama"]
+                   . " | " . $b["peminjam"]
+                   . " | " . $b["status"] . "\n";
+                $no++;
+            }
+        }
+
+
+    } elseif ($pilih == "3") {
+        $idHapus = (int) readline("Masukkan ID barang yang ingin dihapus: ");
+        $idHapus - 1;
+        if ($idHapus == $daftarBarang){
+            array_splice($daftarBarang, $idHapus, 1);
+        } else {
+            echo "Error: barang tidak ditemukan!";
+        }
+    } elseif ($pilih == "4") {
+        echo "Terima kasih!\n";
+        break;
+    } else {
+        echo "Menu tidak dikenali.\n";
+    }
+}
